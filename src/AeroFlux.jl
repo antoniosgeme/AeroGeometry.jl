785cb3f7b8452271eebcebf627e944ae0a2c6dc3
antoniosgeme <antoniosgeme@gmail.com>
2023-05-02 17:52:50 -0400

    added control surface function repanel doenst work

--- src/AeroFlux.jl
+++ src/AeroFlux.jl
@@ -1,12 +1,17 @@
 module AeroFlux
 
 using Reexport
+using Plots
+plotly()
 
 include("./airfoil.jl")
 @reexport using .airfoil
 
 foil = Airfoil("naca6412")
-add_control_surface!(foil,deflection=10)
+fig = plotme(foil)
+repanel!(foil,100)
+scatter!(fig,foil.coordinates[:,1],foil.coordinates[:,2])
+#add_control_surface!(foil,deflection=10)
 
 end # module AeroFlux
 
