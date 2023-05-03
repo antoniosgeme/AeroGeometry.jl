9a0d3cd7db856c767a1e0c9e297028c4100e0f76
Antonios Gementzopoulos <antoniosgeme@gmail.com>
2023-05-02 22:36:30 -0400

    finalized airfoils

--- src/AeroFlux.jl
+++ src/AeroFlux.jl
@@ -1,16 +1,8 @@
 module AeroFlux
 
 using Reexport
-#using Plots
-
 include("./Airfoils.jl")
 @reexport using .Airfoils
-
-#foil = Airfoil("naca6412")
-#fig = plotme(foil)
-#repanel!(foil,100)
-#scatter!(fig,foil.coordinates[:,1],foil.coordinates[:,2])
-#add_control_surface!(foil,deflection=10)
 
 end # module AeroFlux
 
