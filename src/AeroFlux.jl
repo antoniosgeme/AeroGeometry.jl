88a3e0dcf0f6c76156eaca726f1e2970e74b673f
Antonios Gementzopoulos <antoniosgeme@gmail.com>
2023-11-27 13:53:33 -0500

    fixed export

--- src/AeroFlux.jl
+++ src/AeroFlux.jl
@@ -1,18 +1,13 @@
 module AeroFlux
 
 include("./Airfoils.jl")
-export Airfoil, plotme, get_upper_coordinates, get_lower_coordinates, get_area,
+export Airfoil, get_upper_coordinates, get_lower_coordinates, get_area,
        get_centroid, repanel!, write_file, get_local_camber, get_local_thickness,
        get_max_camber, get_max_thickness, get_LE_index, get_TE_thickness,
        get_TE_angle, add_control_surface!, repanel, blend_airfoils
 
 include("./Wings.jl")
 export Wing, WingXSec
-
-#include("./Singularities.jl")
-
-#@reexport using .Singularities
-#export LineVortex
 
 end # module AeroFlux
 
