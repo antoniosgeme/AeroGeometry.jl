81d3e2cffecdfb517b7e8069c26a849d46df66ae
Antonios Gementzopoulos <antoniosgeme@gmail.com>
2023-11-27 22:02:03 -0500

    added wing plotting

--- src/AeroFlux.jl
+++ src/AeroFlux.jl
@@ -4,7 +4,7 @@ include("./Airfoils.jl")
 export Airfoil, get_upper_coordinates, get_lower_coordinates, get_area,
        get_centroid, repanel!, write_file, get_local_camber, get_local_thickness,
        get_max_camber, get_max_thickness, get_LE_index, get_TE_thickness,
-       get_TE_angle, add_control_surface!, repanel, blend_airfoils,get_surface_coordinates
+       get_TE_angle, add_control_surface!,add_control_surface , repanel, blend_airfoils,get_surface_coordinates
 
 include("./Wings.jl")
 export Wing, WingXSec
--- src/Wings.jl
+++ src/Wings.jl
@@ -1,6 +1,8 @@
+using RecipesBase
+
 mutable struct WingXSec
     airfoil::Airfoil
-    xyz_le::Vector{Float64}
+    le_loc::Vector{Float64}
     chord::Float64
     twist::Float64
 end 
@@ -14,4 +16,27 @@ mutable struct Wing
     symmetric::Bool
 end 
 
+@recipe function plot(wing::Wing) 
+    xlabel --> "x" 
+    ylabel --> "y"
+    zlabel --> "z"
+    zlims --> (-2,2)
+    markersize --> 1
+    fillrange --> 0
+    aspect_ratio --> 1
+    legend --> :none
+    for i in eachindex(wing.xsecs)
+        @series begin
+            (
+                wing.xsecs[i].airfoil.coordinates[:,1] .+ wing.xsecs[i].le_loc[1], 
+                zeros(size(wing.xsecs[i].airfoil.coordinates[:,2])) .+ wing.xsecs[i].le_loc[2],
+                wing.xsecs[i].airfoil.coordinates[:,2] .+ wing.xsecs[i].le_loc[3]
+            )
+        end
+    end 
+end
+
+
 get_area(xsec::WingXSec) = get_area(xsec.airfoil) * xsec.chord^2
+
+
