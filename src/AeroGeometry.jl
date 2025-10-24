fc6ab154d27f62209c9f08428ef7c9e790928b12
Antonios Gementzopoulos <antoniosgeme@gmail.com>
2025-10-24 16:17:28 -0400

    Change include statements to use lowercase filenames for cross-platform compatibility

--- src/AeroGeometry.jl
+++ src/AeroGeometry.jl
@@ -6,28 +6,28 @@ import Base.show
 
 abstract type AeroComponent end
 
-include("./Tools.jl")
-include("./Units.jl")
+include("./tools.jl")
+include("./units.jl")
 
-include("./Airfoils.jl")
+include("./airfoils.jl")
 export Airfoil, coordinates, area,
        centroid, repanel!, write_file, camber, thickness,
        max_camber, max_thickness, leading_edge_index, trailing_edge_thickness,
        trailing_edge_angle, deflect_control_surface!,deflect_control_surface , repanel, blend_airfoils, 
        quarter_chord, list_airfoil_names, normals, tangents, surface_coordinates, AeroComponent
 
-include("./Wings.jl")
+include("./wings.jl")
 export Wing, WingSection, ControlSurface, translate!, deflect_control_surface!
 
-include("./Fuselages.jl")
+include("./fuselages.jl")
 export Fuselage, FuselageSection, coordinates, add_xsec!
 
-include("./Airplanes.jl")
+include("./airplanes.jl")
 export Airplane
 
-include("./PlotsViz.jl")
+include("./plotsviz.jl")
 
-include("./Library.jl")
+include("./library.jl")
 export cessna152
 
 function viz end
