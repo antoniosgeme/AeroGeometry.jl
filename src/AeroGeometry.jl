f218bba065708ad30d8f417887e6ac79f60ebc27
Antonios Gementzopoulos <antoniosgeme@gmail.com>
2025-10-25 12:42:41 -0400

    fixed capitals

--- src/AeroGeometry.jl
+++ src/AeroGeometry.jl
@@ -6,28 +6,28 @@ import Base.show
 
 abstract type AeroComponent end
 
-include("./tools.jl")
-include("./units.jl")
+include("./Tools.jl")
+include("./Units.jl")
 
-include("./airfoils.jl")
+include("./Airfoils.jl")
 export Airfoil, coordinates, area,
        centroid, repanel!, write_file, camber, thickness,
        max_camber, max_thickness, leading_edge_index, trailing_edge_thickness,
        trailing_edge_angle, deflect_control_surface!,deflect_control_surface , repanel, blend_airfoils, 
        quarter_chord, list_airfoil_names, normals, tangents, surface_coordinates, AeroComponent
 
-include("./wings.jl")
+include("./Wings.jl")
 export Wing, WingSection, ControlSurface, translate!, deflect_control_surface!
 
-include("./fuselages.jl")
+include("./Fuselages.jl")
 export Fuselage, FuselageSection, coordinates, add_xsec!
 
-include("./airplanes.jl")
+include("./Airplanes.jl")
 export Airplane
 
-include("./plotsviz.jl")
+include("./PlotsViz.jl")
 
-include("./library.jl")
+include("./Library.jl")
 export cessna152
 
 function viz end
